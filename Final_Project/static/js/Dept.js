e62e526370b674a62d945932bd17cdc5d020297b
Ziad Elnady <ziyadahmedza778@gmail.com>
2024-01-04 12:44:23 +0200

    Updates for the whole dashboards

---
+++ Final_Project/static/js/Dept.js
@@ -0,0 +1,21 @@
+let dept = document.querySelectorAll("input[name=department]").value;
+// let dept = document.querySelector(".department").value;
+// let dept2 = document.getElementById("dept2");
+let place = document.querySelector(".dept span");
+let root_theme = document.querySelector(":root");
+
+// if (dept === "dept2") {
+//   place.textContent = "Cardiology";
+// } else {
+//   place.textContent = "ICU";
+//   root_theme.style.setProperty("--color-primary", "#005383");
+//   root_theme.style.setProperty("--color-low", "#006291");
+//   root_theme.style.setProperty("--color-secondary", "#f85a40");
+// }
+
+// dept2.addEventListener("click", () => {
+//   place.textContent = "ICU";
+//   root_theme.style.setProperty("--color-primary", "#005383");
+//   root_theme.style.setProperty("--color-low", "#006291");
+//   root_theme.style.setProperty("--color-secondary", "#f85a40");
+// });
